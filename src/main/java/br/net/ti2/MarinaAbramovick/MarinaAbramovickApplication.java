85e5a4c9958397b3ecd0040fbd790e77953c5ece
tiagoti <tiago@ti2.net.br>
2018-05-30 08:19:41 -0300

    removido ports do docker e adicionado scann config ao spring

--- src/main/java/br/net/ti2/MarinaAbramovick/MarinaAbramovickApplication.java
+++ src/main/java/br/net/ti2/MarinaAbramovick/MarinaAbramovickApplication.java
@@ -3,7 +3,10 @@ package br.net.ti2.MarinaAbramovick;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+
+@ComponentScan(basePackages = "br.net.ti2.MarinaAbramovick")
 @SpringBootApplication
 public class MarinaAbramovickApplication {
 	
