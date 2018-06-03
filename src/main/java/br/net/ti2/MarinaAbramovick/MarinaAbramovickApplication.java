e565812a20a0c8d841306961625f79058dce9be9
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2018-06-03 14:18:25 -0300

    removido boco de comentário

--- src/main/java/br/net/ti2/MarinaAbramovick/MarinaAbramovickApplication.java
+++ src/main/java/br/net/ti2/MarinaAbramovick/MarinaAbramovickApplication.java
@@ -1,6 +1,5 @@
 package br.net.ti2.MarinaAbramovick;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,17 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = "br.net.ti2.MarinaAbramovick")
 @SpringBootApplication
 public class MarinaAbramovickApplication {
-	
-	@Autowired
-	private Marina marina;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(MarinaAbramovickApplication.class, args);
 	}
-	
-	/*@PostConstruct
-	public void teste() {
-		marina.exec();
-	}*/
-	
 }
